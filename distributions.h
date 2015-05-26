942da74b4f19d7f0d42256421777d0383ce3effb
Francis DiTraglia <fditra@sas.upenn.edu>
2015-05-26 15:42:01 -0400

    fix header file

--- distributions.h
+++ distributions.h
@@ -4,12 +4,13 @@
 arma::colvec draw_normal(arma::colvec mu, arma::mat Sigma_inv);
 
 arma::vec density_normal(arma::mat x, arma::colvec mu, 
-                      arma::mat Sigma_inv);
+                      arma::mat Sigma_inv, bool logret = false);
 
 arma::mat draw_wishart(int v, arma::mat S);
 
 double log_mv_gamma(int p, double a);
 
-double density_wishart(arma::mat X, int v, arma::mat S);
+double density_wishart(arma::mat X, int v, arma::mat S, 
+                       bool logret = false);
 
 #endif
